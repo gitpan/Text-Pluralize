63835fa6d70a0830211dc32588c89d2c4e209692
Kevin Michael Vail <kevin@vaildc.net>
2007-08-08 02:20:57 +0000

    Import of KVAIL/Text-Pluralize-1.1 from CPAN.
    gitpan-cpan-distribution: Text-Pluralize
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         KVAIL/Text-Pluralize-1.1.tar.gz
    gitpan-cpan-author:       KVAIL
    gitpan-cpan-maturity:     released

--- lib/Text/Pluralize.pm
+++ lib/Text/Pluralize.pm
@@ -1,130 +1,22 @@
 ##==============================================================================
 ## Text::Pluralize - simple pluralization routine
 ##==============================================================================
-## Copyright 2004 Kevin Michael Vail
-## This program is free software; you can redistribute it and/or modify it under
-## the same terms as Perl itself.
+## $Id: Pluralize.pm,v 1.1 2007/08/08 02:14:03 kevin Exp $
 ##==============================================================================
-## $Id: Pluralize.pm,v 1.0 2004/05/23 05:33:14 kevin Exp $
-##==============================================================================
-require 5.006;
-
+require 5.006001;
 package Text::Pluralize;
 use strict;
 use warnings;
-our ($VERSION) = q$Revision: 1.0 $ =~ /^Revision:\s+(\S+)/ or $VERSION = "0.0";
+our $VERSION = '1.1';
+
 use base qw(Exporter);
-our @EXPORT = qw/pluralize/;
 
-=head1 NAME
+##==============================================================================
+## Exported Items
+##==============================================================================
+our @EXPORT = qw(pluralize);
 
-Text::Pluralize - simple pluralization routine
-
-=head1 SYNOPSIS
-
-	use Text::Pluralize;
-
-	print pluralize("file", $count);
-	print pluralize("%d file(s) copied\n"), $count;
-	print pluralize("There (was|were) {no|one|%d} error(s)\n", $count);
-
-=head1 DESCRIPTION
-
-Text::Pluralize provides a lightweight routine to produce the proper form,
-singular or plural, of a word or phrase. Its intended purpose is to produce
-messages for the user, whether error messages or informational messages, without
-the awkward "1 file(s) copied" appearance.
-
-=head1 EXPORTED ROUTINE
-
-=over 4
-
-=item I<$string> = pluralize(I<$template>, I<$count>);
-
-Returns I<$template> customized by I<$count>. I<$template> may contain items
-matching the following formats:
-
-=over 4
-
-=item C<< (I<s1>|I<pl>) >>
-
-If I<$count> is equal to one, I<s1> will appear here; otherwise I<pl> will
-appear at this point in the output. Either I<s1> or I<pl> can be empty.
-
-=item C<< (I<pl>) >>
-
-If I<$count> is not equal to one, the string I<pl> will appear at this point in
-the output. This is equivalent to C<< (|I<pl>) >>.
-
-=item C<< (I<s1>|I<s2>|...|I<pl>) >>
-
-This can be generalized. I<s1> is used if I<$count> is equal to one, I<s2> if
-the count is equal to two, and so forth; I<pl> is used for anything greater than
-the last specific string applied.
-
-=item C<< {I<s0>|I<s1>|I<pl>} >>
-
-With curly braces, the choices start at zero. I<s0> is used if I<$count> is
-zero, I<s1> if it's one, and I<pl> if it's anything else.
-
-=item C<< {I<s0>|I<s1>|I<s2>|...|I<pl>} >>
-
-As with the parenthesized version, this can be generalized.
-
-=back
-
-If none of the above substitutions appear in I<$template>, it is treated as if
-it ended in C<< (s) >>.
-
-Once the above substitutions have been applied, the result is examined to see if
-it contains any C<%> characters. If so, it is used as a format for
-L<sprintf|perlfunc/sprintf>, with the count and any other arguments passed to
-B<pluralize>. This means that if you have a C<%> in your template that is I<not>
-supposed to be a format character, you must specify C<%%> instead.
-
-=back
-
-=head1 EXAMPLES
-
-In each of the examples below, the first column represents the template, the
-second column the count, and the third column the result.
-
-	item                            0   items
-	                                1   item
-	                                2   items
-
-	item(s) need{|s|} attention     0   items need attention
-	                                1   item needs attention
-	                                2   items need attention
-
-	{No|%d} quer(y|ies) (is|are)    0   No queries are
-	                                1   1 query is
-	                                2   2 queries are
-
-	{No|One|Two|Three|%d} item(s)   0   No items
-	                                1   One item
-	                                2   Two items
-	                                3   Three items
-	                                4   4 items
-
-=head1 NOTE
-
-If the brackets for a substitution don't match up, the one on the left controls
-what happens.
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2004 Kevin Michael Vail
-
-This program is free software; you can redistribute it and/or modify it under
-the same terms as Perl itself.
-
-=head1 AUTHOR
-
-Kevin Michael Vail <F<kevin>@F<vaildc>.F<net>>
-
-=cut
-
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 ##==============================================================================
 ## pluralize
 ##==============================================================================
@@ -144,7 +36,7 @@ sub pluralize ($$;@) {
 			    [)}]			## closing ) or }
 			 ))
 			 (.*)$				## and then the rest of the string
-		/x
+		/xs
 	) {
 		++$control_count;
 		$output .= $1;
@@ -181,11 +73,140 @@ sub pluralize ($$;@) {
 	return $output;
 }
 
+##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 1;
 
-##==============================================================================
-## $Log: Pluralize.pm,v $
-## Revision 1.0  2004/05/23 05:33:14  kevin
-## Initial revision
-##
-##==============================================================================
+__END__
+
+=pod
+
+=for Tk::PodView tuck:0,tabstop:4
+
+=head1 NAME
+
+Text::Pluralize - simple pluralization routine
+
+=head1 SYNOPSIS
+
+	use Text::Pluralize;
+	
+	print pluralize("file", $count);
+	print pluralize("%d file(s) copied\n"), $count;
+	print pluralize("There (was|were) {no|one|%d} error(s)\n", $count);
+
+=head1 DESCRIPTION
+
+C<Text::Pluralize> provides a lightweight routine to produce the proper form, 
+singular or plural, of a word or phrase. Its intended purpose is to produce 
+messages for the user, whether error messages or informational messages, without
+the awkward "1 file(s) copied" appearance.
+
+=head1 EXPORTED ROUTINE
+
+=over 4
+
+=item pluralize
+
+C<< I<$string> = pluralize(I<$template>, I<$count>); >>
+
+Returns I<$template> customized by I<$count>. I<$template> may contain items 
+matching the following formats:
+
+=over 4
+
+=item C<< (I<s1>|I<pl>) >>
+
+If I<$count> is equal to one, I<s1> will appear here; otherwise I<$pl> will 
+appear at this point in the output. Either I<s1> or I<pl> can be empty.
+
+=item C<< (I<pl>) >>
+
+If I<$count> is not equal to one, the string I<pl> will appear at this point in 
+the output. This is equivalent to C<< (|I<pl>) >>.
+
+=item C<< (I<s1>|I<s2>|...|I<pl>) >>
+
+This can be generalized. I<s1> is used if I<$count> is equal to one, I<s2> if 
+the count is equal to two, and so forth; I<pl> is used for anything greater than
+the last specific string applied.
+
+=item C<< {I<s0>|I<s1>|I<pl>} >>
+
+With curly braces, the choices start at zero. I<s0> is used if I<$count> is 
+zero, I<s1> if it's one, and I<pl> if it's anything else.
+
+=item C<< {I<s0>|I<s1>|I<s2>|...|I<pl>} >>
+
+As with the parenthesized version, this can be generalized.
+
+=back
+
+If none of the above substitutions appear in I<$template>, it is treated as if 
+it ended in C<< (s) >>.
+
+Once the above substitutions have been applied, the result is examined to see if
+it contains any C<%> characters. If so, it is used as a format for 
+L<sprintf|perlfunc/sprintf>, with the count and any other arguments passed to 
+B<pluralize>. This means that if you have a C<%> in your template that is I<not>
+supposed to be a format character, you must specify C<%%> instead.
+
+=back
+
+=head1 EXAMPLES
+
+In each of the examples below, the first column represents the template, the 
+second column the count, and the third column the result.
+
+	item                            0   items
+	                                1   item
+	                                2   items
+
+	item(s) need{|s|} attention     0   items need attention
+	                                1   item needs attention
+	                                2   items need attention
+
+	{No|%d} quer(y|ies) (is|are)    0   No queries are
+	                                1   1 query is
+	                                2   2 queries are
+
+	{No|One|Two|Three|%d} item(s)   0   No items
+	                                1   One item
+	                                2   Two items
+	                                3   Three items
+	                                4   4 items
+
+=head1 NOTE
+
+If the brackets for a substitution don't match up, the one on the left controls 
+what happens.
+
+=head1 HISTORY
+
+=for Tk::PodView tuck:1
+
+=over 4
+
+=item 1.0
+
+Initial version
+
+=item 1.1
+
+Fix a problem with format strings containing newlines.
+
+=back
+
+=for Tk::PodView tuck:restore
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Kevin Michael Vail, all rights reserved
+
+This library is free software. You may modify and/or redistribute it under the 
+same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kevin Michael Vail <kvail@cpan.org>
+
